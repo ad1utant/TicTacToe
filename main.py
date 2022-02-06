17da0df63b195f17c5e832594a38e170b3339981
Sebastian Jankowski <38406394+sebastian-j@users.noreply.github.com>
2022-02-06 08:04:40 +0100

    initialize buttons in a for loop
    Buttons are now initialized in a double for loop and are stored in 2d array instead of separate variables for each button.

--- main.py
+++ main.py
@@ -38,151 +38,61 @@ def Sprawdzacz():
             "c1" in kolka and "b2" in kolka and "a3" in kolka):
         info = tk.messagebox.showinfo(title=None, message="Kółko wygrało")
         exit()
-    elif a1['state'] == "disabled" and a2['state'] == "disabled" and a3['state'] == "disabled" and b1['state'] == "disabled" and b2['state'] == "disabled" and b3['state'] == "disabled" and c1['state'] == "disabled" and c2['state'] == "disabled" and c3['state'] == "disabled":
+    elif buttons[0][0]['state'] == "disabled" and buttons[0][1]['state'] == "disabled" and buttons[0][2]['state'] == "disabled" and buttons[1][0]['state'] == "disabled" and buttons[1][1]['state'] == "disabled" and buttons[1][2]['state'] == "disabled" and buttons[2][0]['state'] == "disabled" and buttons[2][1]['state'] == "disabled" and buttons[2][2]['state'] == "disabled":
         info = tk.messagebox.showinfo(title=None, message="Remis")
         exit()
 def Nacisk():
     i=0
-    if a1['state'] == "disabled":
+    if buttons[0][0]['state'] == "disabled":
         i+=1
-    if a2['state'] == "disabled":
+    if buttons[0][1]['state'] == "disabled":
         i+=1
-    if a3['state'] == "disabled":
+    if buttons[0][2]['state'] == "disabled":
         i+=1
-    if b1['state'] == "disabled":
+    if buttons[1][0]['state'] == "disabled":
         i+=1
-    if b2['state'] == "disabled":
+    if buttons[1][1]['state'] == "disabled":
         i+=1
-    if b3['state'] == "disabled":
+    if buttons[1][2]['state'] == "disabled":
         i+=1
-    if c1['state'] == "disabled":
+    if buttons[2][0]['state'] == "disabled":
         i+=1
-    if c2['state'] == "disabled":
+    if buttons[2][1]['state'] == "disabled":
         i+=1
-    if c3['state'] == "disabled":
+    if buttons[2][2]['state'] == "disabled":
         i+=1
     return i
-def a1Nacisk():
+def HandleButtonClick(x, y):
     i = Nacisk()
+    letters = ['a', 'b', 'c']
     if i % 2 == 0:
-        a1['image'] = cross
-        a1["state"] = "disabled"
-        krzyze.append("a1")
+        buttons[x][y]['image'] = cross
+        buttons[x][y]["state"] = "disabled"
+        krzyze.append(letters[x] + str(y + 1))
     else:
-        a1['image'] = circle
-        a1["state"] = "disabled"
-        kolka.append("a1")
-    Sprawdzacz()
-def a2Nacisk():
-    i = Nacisk()
-    if i % 2 == 0:
-        a2['image'] = cross
-        a2["state"] = "disabled"
-        krzyze.append("a2")
-    else:
-        a2['image'] = circle
-        a2["state"] = "disabled"
-        kolka.append("a2")
-    Sprawdzacz()
-def a3Nacisk():
-    i = Nacisk()
-    if i % 2 == 0:
-        a3['image'] = cross
-        a3["state"] = "disabled"
-        krzyze.append("a3")
-    else:
-        a3['image'] = circle
-        a3["state"] = "disabled"
-        kolka.append("a3")
-    Sprawdzacz()
-def b1Nacisk():
-    i = Nacisk()
-    if i % 2 == 0:
-        b1['image'] = cross
-        b1["state"] = "disabled"
-        krzyze.append("b1")
-    else:
-        b1['image'] = circle
-        b1["state"] = "disabled"
-        kolka.append("b1")
-    Sprawdzacz()
-def b2Nacisk():
-    i = Nacisk()
-    if i % 2 == 0:
-        b2['image'] = cross
-        b2["state"] = "disabled"
-        krzyze.append("b2")
-    else:
-        b2['image'] = circle
-        b2["state"] = "disabled"
-        kolka.append("b2")
-    Sprawdzacz()
-def b3Nacisk():
-    i = Nacisk()
-    if i % 2 == 0:
-        b3['image'] = cross
-        b3["state"] = "disabled"
-        krzyze.append("b3")
-    else:
-        b3['image'] = circle
-        b3["state"] = "disabled"
-        kolka.append("b3")
-    Sprawdzacz()
-def c1Nacisk():
-    i = Nacisk()
-    if i % 2 == 0:
-        c1['image'] = cross
-        c1["state"] = "disabled"
-        krzyze.append("c1")
-    else:
-        c1['image'] = circle
-        c1["state"] = "disabled"
-        kolka.append("c1")
-    Sprawdzacz()
-def c2Nacisk():
-    i = Nacisk()
-    if i % 2 == 0:
-        c2['image'] = cross
-        c2["state"] = "disabled"
-        krzyze.append("c2")
-    else:
-        c2['image'] = circle
-        c2["state"] = "disabled"
-        kolka.append("c2")
-    Sprawdzacz()
-def c3Nacisk():
-    i = Nacisk()
-    if i % 2 == 0:
-        c3['image'] = cross
-        c3["state"] = "disabled"
-        krzyze.append("c3")
-    else:
-        c3['image'] = circle
-        c3["state"] = "disabled"
-        kolka.append("c3")
+        buttons[x][y]['image'] = circle
+        buttons[x][y]["state"] = "disabled"
+        kolka.append(letters[x] + str(y + 1))
     Sprawdzacz()
 ##
-a1 = tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=a1Nacisk)
-a2 = tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=a2Nacisk)
-a3 = tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=a3Nacisk)
-b1 = tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=b1Nacisk)
-b2 = tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=b2Nacisk)
-b3 = tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=b3Nacisk)
-c1 = tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=c1Nacisk)
-c2 = tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=c2Nacisk)
-c3 = tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=c3Nacisk)
+buttons = []
+for j in range(3):
+    buttonRow = []
+    for k in range(3):
+        buttonRow.append(tk.Button(root, height=40, width=40, bg="#F7F7F7", image=blank, command=lambda j=j, k=k: HandleButtonClick(j, k)))
+    buttons.append(buttonRow)
 ##
-def Gridy(a1, a2, a3, b1, b2, b3, c1, c2, c3):
-    a1.grid(ipady=30, ipadx=30, padx=5, pady=5, column=1, row=1)
-    a2.grid(ipady=30, ipadx=30, padx=5, pady=5, column=1, row=2)
-    a3.grid(ipady=30, ipadx=30, padx=5, pady=5, column=1, row=3)
-    b1.grid(ipady=30, ipadx=30, padx=5, pady=5, column=2, row=1)
-    b2.grid(ipady=30, ipadx=30, padx=5, pady=5, column=2, row=2)
-    b3.grid(ipady=30, ipadx=30, padx=5, pady=5, column=2, row=3)
-    c1.grid(ipady=30, ipadx=30, padx=5, pady=5, column=3, row=1)
-    c2.grid(ipady=30, ipadx=30, padx=5, pady=5, column=3, row=2)
-    c3.grid(ipady=30, ipadx=30, padx=5, pady=5, column=3, row=3)
-    return a1, a2, a3, b1, b2, b3, c1, c2, c3
+def Gridy(buttons):
+    buttons[0][0].grid(ipady=30, ipadx=30, padx=5, pady=5, column=1, row=1)
+    buttons[0][1].grid(ipady=30, ipadx=30, padx=5, pady=5, column=1, row=2)
+    buttons[0][2].grid(ipady=30, ipadx=30, padx=5, pady=5, column=1, row=3)
+    buttons[1][0].grid(ipady=30, ipadx=30, padx=5, pady=5, column=2, row=1)
+    buttons[1][1].grid(ipady=30, ipadx=30, padx=5, pady=5, column=2, row=2)
+    buttons[1][2].grid(ipady=30, ipadx=30, padx=5, pady=5, column=2, row=3)
+    buttons[2][0].grid(ipady=30, ipadx=30, padx=5, pady=5, column=3, row=1)
+    buttons[2][1].grid(ipady=30, ipadx=30, padx=5, pady=5, column=3, row=2)
+    buttons[2][2].grid(ipady=30, ipadx=30, padx=5, pady=5, column=3, row=3)
+    return buttons
 ##
-gridy = Gridy(a1, a2, a3, b1, b2, b3, c1, c2, c3)
+gridy = Gridy(buttons)
 root.mainloop()
